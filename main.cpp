6d88955d6c6bda2ed97bb4c13aea071671551b91
Eckaos <nathan.pollart@hotmail.fr>
2021-10-09 16:39:08 +0200

    add stylesheet

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <QApplication>
 #include <mainWidget.hpp>
 #include <QScreen>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[]){
 	QApplication app(argc, argv);
+	QFile file("stylesheet.qss");
+	file.open(QFile::ReadOnly);
+	QString styleSheet = QLatin1String(file.readAll());
+	app.setStyleSheet(styleSheet);
 	MainWidget w;	
-	QRect rec = app.primaryScreen()->availableGeometry();
-	w.resize(rec.width(),rec.height());
+	w.setWindowState(Qt::WindowMaximized);
 	w.show();
 	app.setWindowIcon(QIcon("Remembrall-2-icon.png"));
 
---
+++ main.qml
@@ -0,0 +1,8 @@
+import QtQuick 2.6
+import QtQuick.Controls 2.0
+import io.qt.examples.backend 1.0
+
+ApplicationWindow {
+	id: app
+
+}
