7b9642ece506a07137b433096b2a0d3299162cd0
Eckaos <nathan.pollart@hotmail.fr>
2021-10-09 17:57:15 +0200

    just annuling previous add

--- main.cpp
+++ main.cpp
@@ -6,9 +6,6 @@
 int main(int argc, char *argv[]){
 	QApplication app(argc, argv);
 	QFile file("stylesheet.qss");
-	file.open(QFile::ReadOnly);
-	QString styleSheet = QLatin1String(file.readAll());
-	app.setStyleSheet(styleSheet);
 	MainWidget w;	
 	w.setWindowState(Qt::WindowMaximized);
 	w.show();
